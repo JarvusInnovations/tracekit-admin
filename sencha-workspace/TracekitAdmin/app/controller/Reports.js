ba9bb1c9835bbf2be5c6f2bef29eb4cb62cb9c3a
Bill Clinton <BillClinton@users.noreply.github.com>
2016-03-03 21:34:49 -0500

    Add controller

---
+++ sencha-workspace/TracekitAdmin/app/controller/Reports.js
@@ -0,0 +1,60 @@
+/*jslint browser: true, undef: true *//*global Ext*/
+Ext.define('TracekitAdmin.controller.Reports', {
+    extend: 'Ext.app.Controller',
+
+    // entry points
+    control: {
+        'report-grid': {
+            select: 'onReportGridSelected'
+        },
+        'report-stackgrid': {
+            select: 'onReportStackGridSelected'
+        }
+    },
+
+    listen: {
+        store: {
+             '#Stacks': {
+                 load: 'onStacksStoreLoad'
+             }
+         }
+    },
+
+
+    // controller configuration
+    stores: [
+        'Reports',
+        'Stacks'
+    ],
+
+    views: [
+        'report.Frame',
+        'report.Grid',
+        'report.StackGrid',
+        'report.StackView'
+    ],
+
+    refs: [{
+        ref : 'stackView',
+        selector: 'report-stackview'
+    }],
+
+
+    // event handlers
+    onStacksStoreLoad: function() {
+        this.getStackView().update({stack: ''});
+    },
+
+    onReportGridSelected: function(grid,rec) {
+        this.getStacksStore().load({
+            params: {
+                error_report_id: rec.getId()
+            }
+        });
+    },
+
+    onReportStackGridSelected: function(grid,rec) {
+        this.getStackView().update({stack: rec.get('context')});
+    }
+
+});
